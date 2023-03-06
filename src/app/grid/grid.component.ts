c3354f79ec6b1f9940566c4df7269991b46d7153
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2023-03-06 13:16:51 +0100

    refactor: move color selection code out of html

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -31,7 +31,21 @@ export class GridComponent {
     // The major axis must be aligned with the window aspect ratio
     this.gridMajorWidth = Math.round(width / 100);
     this.gridMajorHeight = Math.round(this.gridMajorWidth * aspectRatio);
+  }
 
-    console.log(this.gridMajorWidth, this.gridMajorHeight);
-    }
+  cellIndex(mar: number, mac: number, mir: number, mic: number): number {
+    return (mar * this.gridMajorWidth + mac + (mir * this.gridMinorWidth + mic) * this.gridMajorWidth * this.gridMajorHeight) - 500;
+  }
+
+  isRed(mar: number, mac: number, mir: number, mic: number): boolean {
+    return this.cellIndex(mar, mac, mir, mic) % 211 === 0;
+  }
+
+  isBlue(mar: number, mac: number, mir: number, mic: number): boolean {
+    return this.cellIndex(mar, mac, mir, mic) % 307 === 0;
+  }
+
+  isYellow(mar: number, mac: number, mir: number, mic: number): boolean {
+    return this.cellIndex(mar, mac, mir, mic) % 347 === 0;
+  }
 }
